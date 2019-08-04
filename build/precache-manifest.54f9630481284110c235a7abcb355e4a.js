260732d4e92262fd8d73e1ab831e12e5a18bcde3
Radomir Wojtera <rwojtera@egnyte.com>
2019-08-04 23:39:24 +0200

    readme

---
+++ build/precache-manifest.54f9630481284110c235a7abcb355e4a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac32488e6d9c6481821e654574969d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb82d43546c4a1e5712c",
+    "url": "./static/css/main.c30bd0a2.chunk.css"
+  },
+  {
+    "revision": "a396616c83f38402d9fe",
+    "url": "./static/js/10.97975a80.chunk.js"
+  },
+  {
+    "revision": "cb82d43546c4a1e5712c",
+    "url": "./static/js/main.9c87b503.chunk.js"
+  },
+  {
+    "revision": "8cf290842fa04abf23ed",
+    "url": "./static/js/runtime~main.ef073fe0.js"
+  },
+  {
+    "revision": "58122246edcfc643cce0",
+    "url": "./static/js/screen-0--Credits-Credits.67c041ee.chunk.js"
+  },
+  {
+    "revision": "a85920000a3033eddb05",
+    "url": "./static/js/screen-10--Menu-Menu.6b3ab91d.chunk.js"
+  },
+  {
+    "revision": "c99e5e0c1ee1d20497b8",
+    "url": "./static/js/screen-12--NotFound-NotFound.a7c5028a.chunk.js"
+  },
+  {
+    "revision": "6e20b21d357db73dcd7a",
+    "url": "./static/js/screen-14--Options-Options.6d02fa53.chunk.js"
+  },
+  {
+    "revision": "365f7cc5f42e7e6fba45",
+    "url": "./static/js/screen-2--GameoverLoser-GameoverLoser.3dbb73a2.chunk.js"
+  },
+  {
+    "revision": "0fd24db934ed69b93ac8",
+    "url": "./static/js/screen-4--GameoverWinner-GameoverWinner.e8c37c54.chunk.js"
+  },
+  {
+    "revision": "92393af3e2faa469418a",
+    "url": "./static/js/screen-6--Gameplay-Gameplay.a4e33fd8.chunk.js"
+  },
+  {
+    "revision": "35973175d5a88cad7a32",
+    "url": "./static/js/screen-8--Leaderboard-Leaderboard.1b6a13eb.chunk.js"
+  }
+]);
