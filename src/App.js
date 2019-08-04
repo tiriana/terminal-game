245f5e78fcd84d80f28e5948cfe5ddac57abb5af
Radomir Wojtera <rwojtera@egnyte.com>
2019-08-04 09:51:04 +0200

    Routing and dynamically loaded screens

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { dynamicScreen } from "./utils/lazy";
+
+import Welcome from "./components/screens/Welcome/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/menu" component={dynamicScreen("Menu", { prefetch: true })} />
+        <Route exact path="/options" component={dynamicScreen("Options")} />
+        <Route exact path="/credits" component={dynamicScreen("Credits")} />
+        <Route path="/leaderboard" component={dynamicScreen("Leaderboard", { prefetch: true })} />
+        <Route path="/play" component={dynamicScreen("Gameplay")} />
+        <Route path="/winner" component={dynamicScreen("GameoverWinner")} />
+        <Route path="/loser" component={dynamicScreen("GameoverLoser")} />
+        <Route component={dynamicScreen("NotFound")} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/utils/lazy.js
@@ -0,0 +1,21 @@
+import React, { Suspense, lazy } from "react";
+
+import { Loading } from "../components/Loading/Loading";
+const loading = <Loading />;
+
+export const dynamicScreen = (screenName, { prefetch = true } = {}) => {
+  const screenFactory = () =>
+    import(/* webpackChunkName: "[request]" */ `../components/screens/${screenName}/${screenName}`);
+
+  const MyAsyncScreen = lazy(screenFactory);
+  const MySuspendedScreen = props => (
+    <Suspense fallback={loading}>
+      <MyAsyncScreen {...props} />
+    </Suspense>
+  );
+
+  MySuspendedScreen.prefetch = screenFactory;
+  prefetch && screenFactory();
+
+  return MySuspendedScreen;
+};
